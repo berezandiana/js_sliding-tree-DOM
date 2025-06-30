ab107eca114fb2f908a755c9c354a29cc4039151
Diana Berezan <berezandiana24@gmail.com>
2025-06-30 12:07:33 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+const allLi = document.querySelectorAll('li');
+
+allLi.forEach((li) => {
+  const childUl = li.querySelector('ul');
+
+  if (childUl) {
+    const firstChild = li.firstChild;
+
+    if (firstChild.nodeType === Node.TEXT_NODE) {
+      const span = document.createElement('span');
+
+      span.textContent = firstChild.textContent.trim();
+      li.insertBefore(span, firstChild);
+      li.removeChild(firstChild);
+
+      span.addEventListener('click', () => {
+        childUl.hidden = !childUl.hidden;
+      });
+    }
+  }
+});
